3834fd7aa4e3bdbf9280a7150205754dfaabefb6
Dmitry <43281522+KartonDCP@users.noreply.github.com>
2020-08-26 19:52:40 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,32 +3,40 @@
 #include <GL/glut.h>
 #include <GL/glu.h>
 
+
+void DrawTriangle(GLfloat x1, GLfloat y1, GLfloat x2, GLfloat y2, GLfloat x3, GLfloat y3, float r, float g, float b)
+{
+    glBegin(GL_TRIANGLES);
+
+    glColor3f(r, g, b);
+
+    glVertex2f(x1, y1);
+    glVertex2f(x2, y2);
+    glVertex2f(x3, y3);
+
+    glEnd();
+}
+
+
+
+
 void init2D(float r, float g, float b)
 {
     glClearColor(r,g,b,0.0);
     glMatrixMode (GL_PROJECTION);
-    glOrtho(0.0, 200.0, 0.0, 150.0, -1, 1);
-
+    glOrtho(0, 640, 480, 0, -1, 1);
 }
 
 void display(void)
 {
+    glClearColor(0,0,0,0);
+
     glClear(GL_COLOR_BUFFER_BIT);
-    glColor3f(1.0, 0.0, 0.0);
-
-    //draw two points
-    glBegin(GL_POINTS);
-    for(int i = 0; i < 10; i++)
+    for (int i = -100; i < 200; i+=30)
     {
-        glVertex2i(10+5*i,110);
+        DrawTriangle(300.0 + i,  210.0 + i, 340.0 + i, 215.0 + i, 320.0 + i, 250.0 + i, 0.5, 100 + i/10, 100 + i/ 5);
     }
-    glEnd();
 
-    //draw a line
-    glBegin(GL_LINES);
-    glVertex2i(10,10);
-    glVertex2i(100,100);
-    glEnd();
 
     glFlush();
 }
