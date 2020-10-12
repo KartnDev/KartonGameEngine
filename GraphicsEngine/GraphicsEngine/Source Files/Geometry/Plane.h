c42063b29f9647a0e8d276fadfa39b88af6d7b05
Dmitry <43281522+KartonDCP@users.noreply.github.com>
2020-10-12 23:59:28 +0300

    update plane

--- GraphicsEngine/GraphicsEngine/Source Files/Geometry/Plane.h
+++ GraphicsEngine/GraphicsEngine/Source Files/Geometry/Plane.h
@@ -1,6 +1,27 @@
 ﻿#pragma once
+#include "Triangle.h"
+#include "../Math Utilities/vector3d.h"
+#include "../Math Utilities/vector2d.h"
+#include "Point3D.h"
 
-struct Plane
+using namespace KtStd::Math;
+using namespace KtStd::Geometry;
+
+struct PlaneVectorized
 {
-	
+	Vector3d normalPlane;
+	Vector3d directVector;
+};
+
+struct Plane3Points
+{
+	Point3D point1;
+	Point3D point2;
+	Point3D point3;
+};
+
+
+struct PlaneTriangle
+{
+	Triangle planeTri;
 };
